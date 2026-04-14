f0e2d500c3e11f7a2fb429bce352a725ec1668b7
Mishanyacomeover <qqewqew900@gmail.com>
2026-04-14 21:12:15 +0300

    FINALSUKA

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <list>
 using namespace std;
 
+// структура для задачи
 struct task {
     int id;
     int value;
@@ -11,106 +12,127 @@ struct task {
     task* next;
 };
 
+// голову и хвост храним глобально
 task* head = nullptr;
 task* tail = nullptr;
 
+//реестр задач
 map <int, task*> registery;
 
 void add(int id, int value) {
+
     task* newTask = new task{ id, value, tail, nullptr };
+
     if (tail) {
-        tail->next = newTask;
-        tail = newTask;
+        tail->next = newTask; //tail теперь указывает на newTask
     }
     else {
-        head = newTask;
-        tail = newTask;
+        head = newTask; //если список был пустым, то head = newTask
     }
-    registery[id] = newTask;
+
+    tail = newTask; // обновляем tail
+    registery[id] = newTask; //добавляем в регистр
 }
 
 void insert_after(int target_id, int id, int value) {
-    auto target = registery[target_id];
-    if (!target) {
+
+    auto target_it = registery.find(target_id); //берем ИТЕРАТОР на задачу
+
+    if (target_it == registery.end()) { // микро проверка на аутизм
         cout << "target не существует" << endl;
         exit(0);
     }
-    task* newTask = new task{ id,value,nullptr,target->next };
-    target->next = newTask;
+
+    task* target = target_it -> second; // превращаем сраный итератор в УКАЗАТЕЛЬ
+
+    task* newTask = new task{id, value, target, target->next};//новый task с нулевым началом и концом у target
+    target->next = newTask; //target теперь указывает на newTask
+
     if (newTask->next) {
         newTask->next->prev = newTask;
+        //У узла, который раньше был следующим после target,теперь предыдущим становится newTask 
     }
-
-    else {
-        tail = newTask;
-    }
-
-    registery[id] = newTask;
 }
 
 void move_to_front(int id) {
-    task* task = registery[id];
-    if (!task || task == head) return;
+
+    auto it = registery.find(id); //итератор на task
+    if (it == registery.end()) return;
+    
+    task* task = it->second; //указатель на task
+    if (task == head) return;
+    
+    //вырезаем task из текущей позиции
     if (task->prev) task->prev->next = task->next;
     if (task->next) task->next->prev = task->prev;
-
+    
+    //если task был хвостом, обновляем хвост
     if (task == tail) tail = task->prev;
-
+    
+    //вставляем task в начало
     task->prev = nullptr;
     task->next = head;
-
-    if (head) head->prev = task; // по идее можно и без проверки, но пусть лучше будет
-
+    
+    //бывшая голова теперь ссылается на task как на предыдущий
+    if (head) head->prev = task;
     head = task;
 }
 
 void splice_range(int id_from, int id_to) {
 
-    task* task_start = registery[id_from];
-    task* task_end = registery[id_to];
-
-    if (!task_start || !task_end || id_from > id_to) return;
-
+    auto it_start = registery.find(id_from);
+    auto it_end = registery.find(id_to);
+    
+    if (it_start == registery.end() || it_end == registery.end()) return;
+    
+    task* task_start = it_start->second;
+    task* task_end = it_end->second;
+    
     if (task_start == task_end) {
         move_to_front(id_from);
         return;
     }
-
-
-
+    
+    // соединяем то, что было до и после вырезаемого диапазона
     if (task_start->prev) task_start->prev->next = task_end->next;
     if (task_end->next) task_end->next->prev = task_start->prev;
-
+    
+    // если вырезаем хвост, обновляем tail
     if (task_end == tail) tail = task_end->prev;
-
+    
+    // вставляем вырезанный диапазон в начало
     task_start->prev = nullptr;
     task_end->next = head;
-
+    
+    // бывшая голова теперь ссылается на начало диапазона
     if (head) head->prev = task_start;
-
     head = task_start;
 }
 
 void get_value(int id) {
-    if (registery[id]) cout << "Значение:" << registery[id]->value << endl;
-    else {
+    auto it = registery.find(id);
+    if (it != registery.end()) {
+        cout << "Значение:" << it->second->value << endl;
+    } else {
         cout << "Задачи не существует" << endl;
-        return;
     }
 }
 
 void remove(int id) {
-    task* task = registery[id];
-    if (!task) return;
-
+    auto it = registery.find(id); //сначала итератор
+    if (it == registery.end()) return;
+    task* task = it->second; //затем указатель...
+    
+    //вырезаем из списка
     if (task->prev) task->prev->next = task->next;
     if (task->next) task->next->prev = task->prev;
-
+    
+    //обновляем голову и хвост
     if (task == head) head = task->next;
     if (task == tail) tail = task->prev;
-
-    registery.erase(id);
-    delete task;
+    
+    registery.erase(it); //удаляем из реестра
+    delete task; //удаляем из памяти
 }
 
 int main() {
@@ -125,7 +147,7 @@ int main() {
         }
 
         else {
-            for (int i = 0; i <= n; i++) {
+            for (int i = 0; i < n; i++) {
                 add(i, i * 10);
                 cout << "Added id:" << i << " value:" << i * 10 << endl;
             }
@@ -134,7 +156,7 @@ int main() {
     }
 
     cout << "Выберите следующее действие:" << endl
-        << "1 - addё <id> <value> " << endl
+        << "1 - add <id> <value> " << endl
         << "2 - insert_after <target_id> <new_id> <value>" << endl
         << "3 - move_to_front <id>" << endl
         << "4 - splice_range <id_from> <id_to>" << endl
@@ -147,6 +169,7 @@ int main() {
     while (true) {
         cin >> choose;
         switch (choose) {
+            
         case 1: {
             int id, value = 0;
             cout << "Введите <id>: ";
@@ -154,7 +177,7 @@ int main() {
             cout << "Введите <value>: ";
             cin >> value;
 
-            if (!registery[id]) {
+            if (registery.find(id) == registery.end()) {
                 add(id, value);
                 cout << "Задача с id = " << id << " и value = " << value << " успешно добавлена!" << endl;
             }
